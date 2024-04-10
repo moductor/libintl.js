d5d827497ac7707ecd7b4565870d6173c70019af
Vojtěch Perník <develop@pervoj.cz>
2024-04-10 19:39:50 +0200

    Add code to load the shared library

---
+++ src/library.ts
@@ -0,0 +1,63 @@
+import { existsSync, readdirSync } from "fs";
+import { IKoffiLib, load } from "koffi";
+import { basename } from "path";
+
+let lib: IKoffiLib | undefined;
+export function init(file?: string) {
+  if (lib) return;
+  file ??= getLibFile();
+
+  if (!file) throw new Error("error finding a library file");
+  lib = load(file);
+}
+
+export function getLib() {
+  init();
+  return lib!;
+}
+
+function getLibFile() {
+  const platform = getPlatformName();
+  return findLibFile(dirs[platform], extensions[platform]);
+}
+
+function getPlatformName() {
+  if (process.platform === "win32") {
+    return "windows";
+  } else if (process.platform === "darwin") {
+    return "macos";
+  }
+  return "linux";
+}
+
+function findLibFile(paths: string[], extension: string) {
+  return listFiles(paths).find((file) => {
+    const name = basename(file);
+    for (const lib of libs) {
+      if (name.startsWith(`${lib}.${extension}`)) return true;
+    }
+    return false;
+  });
+}
+
+function listFiles(paths: string[]) {
+  const files: string[] = [];
+  paths
+    .filter((path) => existsSync(path))
+    .forEach((dir) => files.push(...readdirSync(dir)));
+  return files;
+}
+
+const libs = ["libglib-2.0", "libintl", "libc"];
+
+const dirs = {
+  linux: ["/usr/lib64", "/usr/local/lib64", "/usr/lib", "/usr/local/lib"],
+  windows: process.env["PATH"]!.split(";"),
+  macos: ["/usr/lib", "/usr/local/lib"],
+};
+
+const extensions = {
+  linux: "so",
+  windows: "dll",
+  macos: "dylib",
+};
